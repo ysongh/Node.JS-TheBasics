fc50d64bfd5e4ab4d8890eebdec949189ad86523
Song <yousonghou@yahoo.com>
2018-11-08 05:30:01 +0000

    fix bugs on redirecting

--- routes.js
+++ routes.js
@@ -3,14 +3,16 @@ const reqHandler = (req, res) => {
     const method = req.method;
     
     if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Home</title></head>');
-        res.write('<body><form action="/create-user" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     
     if(url === '/users'){
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Users</title></head>');
         res.write('<body>');
@@ -31,6 +33,7 @@ const reqHandler = (req, res) => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split('=')[1];
             console.log(message);
+            res.statusCode = 302;
             res.setHeader('Location', '/');
             return res.end();
         });
